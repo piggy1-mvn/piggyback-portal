3a56d71a3a23c3800f758ccf4adeabae88a5dcff
Rajan Gupta <rajan0493@gmail.com>
2019-09-13 12:52:31 +0800

    Remove commented import

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import Header from "./common/Header";
 import { PrivateRoute } from "./common/PrivateRoute";
 import Login from "./login/LoginPage";
 import About from "./about/AboutPage";
