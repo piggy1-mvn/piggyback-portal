ae9330ba3d0c7b58e03ab46e103c33d54c437922
Rajan Gupta <rajan0493@gmail.com>
2019-09-20 16:39:57 +0800

    Change to point to Dev Backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
-export const baseUrlUserApi = "http://localhost:8083/user/";
-export const baseUrlPartnerApi = "http://localhost:8085/partner";
-export const baseUrlOrderApi = "http://localhost:8085/order";
+export const baseUrlUserApi = "http://35.222.231.249:8083/user/";
+export const baseUrlPartnerApi = "http://35.222.231.249:8085/partner";
+export const baseUrlOrderApi = "http://35.222.231.249:8085/order";
 export const toastDuration = 3000;
 export const extendUserSessionMinutes = 10;
 export const partnerUserRoles = ["PARTNER_ADMIN", "PARTNER_USER", "PARTNER_API_USER"];
