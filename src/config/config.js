02a98b8f60bde7d0ef02ff44d899261f93dd3a02
Rajan Gupta <rajan0493@gmail.com>
2019-09-30 13:04:49 +0800

    Order Endpoint

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@ const environmentVariable = process.env.REACT_APP_ENV;
 
 export const baseUrlUserApi = (environmentVariable === "dev") ? "http://108.59.87.44:8083/user/" : "http://34.87.47.196:8080/user/";
 export const baseUrlPartnerApi = (environmentVariable === "dev") ? "http://108.59.87.44:8085/partner" : "http://35.198.202.184:8080/partner";
-export const baseUrlOrderApi = (environmentVariable === "dev") ? "http://108.59.87.44:8088/order" : "http://35.186.149.29:8080//order";
+export const baseUrlOrderApi = (environmentVariable === "dev") ? "http://108.59.87.44:8088/order" : "http://35.186.149.29:8080/order";
 export const baseUrlInvoiceApi = (environmentVariable === "dev") ? "http://108.59.87.44:8084/invoice" : "http://35.198.251.69:8080/invoice";
 
 // export const baseUrlUserApi = "http://localhost:8083/user/";
