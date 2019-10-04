0f273d50846f61054dedb7dd54b05e4d5065b55d
Dipty Ojha <e0401948@u.nus.edu>
2019-10-04 23:06:30 +0800

    new dev env setup

--- src/config/config.js
+++ src/config/config.js
@@ -6,10 +6,10 @@ export const authNetApiPass = "855NG4q7W6x5fbuH";
 
 const environmentVariable = process.env.REACT_APP_ENV;
 
-export const baseUrlUserApi = (environmentVariable === "dev") ? "http://108.59.87.44:8083/user/" : "http://34.87.47.196:8080/user/";
-export const baseUrlPartnerApi = (environmentVariable === "dev") ? "http://108.59.87.44:8085/partner" : "http://35.198.202.184:8080/partner";
-export const baseUrlOrderApi = (environmentVariable === "dev") ? "http://108.59.87.44:8088/order" : "http://35.186.149.29:8080/order";
-export const baseUrlInvoiceApi = (environmentVariable === "dev") ? "http://108.59.87.44:8084/invoice" : "http://35.198.251.69:8080/invoice";
+export const baseUrlUserApi = (environmentVariable === "dev") ? "http://35.226.50.19:8083/user/" : "http://34.87.47.196:8080/user/";
+export const baseUrlPartnerApi = (environmentVariable === "dev") ? "http://35.226.50.19:8085/partner" : "http://35.198.202.184:8080/partner";
+export const baseUrlOrderApi = (environmentVariable === "dev") ? "http://35.226.50.19:8088/order" : "http://35.186.149.29:8080/order";
+export const baseUrlInvoiceApi = (environmentVariable === "dev") ? "http://35.226.50.19:8084/invoice" : "http://35.198.251.69:8080/invoice";
 
 // export const baseUrlUserApi = "http://localhost:8083/user/";
 // export const baseUrlPartnerApi = "http://localhost:8085/partner";
